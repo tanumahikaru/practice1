de5fccee643795c8964baac511d9583528f600f4
tanuma <h.tanuma.sys22@morijyobi.ac.jp>
2022-09-15 10:23:55 +0900

    pra2.javaに出力処理追加2

--- pra2.java
+++ pra2.java
@@ -1,6 +1,8 @@
 
-public class Hello{
+public class pra2{
      public static void main(String[] args) {
+	for(int i = 1; i<=100; i++){     
         System.out.println("Hello World");
+	}
     }
 }
