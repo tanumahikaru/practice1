0caaaa2c746df263f8cbc604b6f74eb751583ba3
tanuma <h.tanuma.sys22@morijyobi.ac.jp>
2022-09-15 10:46:10 +0900

    pra2.javaに出力処理に追加
    Hello Worldを100回出力

--- pra2.java
+++ pra2.java
@@ -2,7 +2,8 @@
 public class pra2{
      public static void main(String[] args) {
 	for(int i = 1; i<=100; i++){     
-        System.out.println("Hello World");
+       
+	System.out.println("Hello World");
 	}
     }
 }
